1dd5429a6320ce3fb0ba53e1d148e6b1eac94e14
Jeremy Brown <jeremy.r.brown@gmail.com>
2013-04-17 16:54:01 +0100

    v0.22 output log file
    Start using hunspell-xml-0.22.jar
    
    Create a log file next to the xml input file, with the contents of the
    info, error, and warning logs.
    
    HunspellXMLConverter.convert should be run outside of the UI thread.

--- griffon-app/controllers/hunspellxml/HunspellXMLController.groovy
+++ griffon-app/controllers/hunspellxml/HunspellXMLController.groovy
@@ -132,65 +132,76 @@ class HunspellXMLController {
 	
 	def convert(file)
 	{
-		model.statusLog = ""
-		
-		model.statusLog += "Opening file ${file.getCanonicalPath()}<br>\r\n"
-		
-		model.options.customPath = view.customPath.text
-		model.options.hunspellFileName = view.hunspellFileName.text
-		if(model.hunspellFileNameUseInput)
-		{
-			def hunName = file.getName().replaceAll(/\.[xX][mM][lL]$/, "")
-			model.options.hunspellFileName = hunName
-		}
-		
-		try
-		{
-			//Validate, convert, and export the Hunspell dictionary
-			model.statusLog += "Validate HunspellXML...<br>\r\n"
-			def hxc = new HunspellXMLConverter(file, model.options)
-			hxc.log.infoLogHook = {message-> model.statusLog += """<font color="#000099">$message</font><br>\r\n"""}
-			hxc.log.errorLogHook = {message-> model.statusLog += formatError("ERROR:<pre>$message</pre>")}
-			hxc.log.warningLogHook = {message->
-				message = message.replaceAll(/\r\n\{\r\n/, "\r\n<ul>\r\n")
-				message = message.replaceAll(/\r\n\}\r\n/, "\r\n</ul>\r\n")
-				for(i in 1..2)
+		doOutside{
+			model.statusLog = ""
+			
+			model.statusLog += "Opening file ${file.getCanonicalPath()}<br>\r\n"
+			
+			model.options.customPath = view.customPath.text
+			model.options.hunspellFileName = view.hunspellFileName.text
+			if(model.hunspellFileNameUseInput)
+			{
+				def hunName = file.getName().replaceAll(/\.[xX][mM][lL]$/, "")
+				model.options.hunspellFileName = hunName
+			}
+			
+			new File(file.toString().replaceAll(/\.xml$/, "_HunspellXML-Converter.log")).withWriter("UTF-8"){logWriter->
+				try
 				{
-					message = message.replaceAll(/\r\n\t(.*)\r\n/, "\r\n<li>\$1</li>\r\n")
-				}
-				model.statusLog += formatWarning("WARNING: $message")
-			}
-			hxcLog = hxc.log
+					//Validate, convert, and export the Hunspell dictionary
+					model.statusLog += "Validate HunspellXML...<br>\r\n"
+					def hxc = new HunspellXMLConverter(file, model.options)
+					hxc.log.infoLogHook = {message-> 
+						logWriter << "INFO: " + message + "\r\n"
+						model.statusLog += """<font color="#000099">$message</font><br>\r\n"""
+					}
+					hxc.log.errorLogHook = {message-> 
+						logWriter << "ERROR: " + message + "\r\n"
+						model.statusLog += formatError("ERROR:<pre>$message</pre>")
+					}
+					hxc.log.warningLogHook = {message->
+						logWriter << "WARNING: " + message + "\r\n"
+						message = message.replaceAll(/\r\n\{\r\n/, "\r\n<ul>\r\n")
+						message = message.replaceAll(/\r\n\}\r\n/, "\r\n</ul>\r\n")
+						for(i in 1..2)
+						{
+							message = message.replaceAll(/\r\n\t(.*)\r\n/, "\r\n<li>\$1</li>\r\n")
+						}
+						model.statusLog += formatWarning("WARNING: $message")
+					}
+					hxcLog = hxc.log
 
-			hxc.convert()
-			
-			//Run tests
-			if(hxc?.tester)
-			{
-				model.tester = hxc.tester
+					hxc.convert()
+					
+					//Run tests
+					if(hxc?.tester)
+					{
+						model.tester = hxc.tester
+					}
+					
+					if(hxc?.parser?.check?.checkMap)
+					{
+						hxcLog.info("List of affixation paths in your dictionary:")
+					}
+					for(entry in hxc?.parser?.check?.checkMap)
+					{
+						def route = entry.value.route
+						StringBuffer sb = new StringBuffer()
+						hxcLog.info(hxc.parser.check.shortFormatRoute(route, true))
+					}
+					
+					
+					model.statusLog += model.startingMessage
+					
+					view.spellCheck.checkSpelling()
+				}
+				catch(Exception e)
+				{
+					model.statusLog += formatError("ERROR:<pre>${StackTrace.getStackTrace(e)}</pre>")
+				}
 			}
-			
-			if(hxc?.parser?.check?.checkMap)
-			{
-				hxcLog.info("List of affixation paths in your dictionary:")
-			}
-			for(entry in hxc?.parser?.check?.checkMap)
-			{
-				def route = entry.value.route
-				StringBuffer sb = new StringBuffer()
-				hxcLog.info(hxc.parser.check.shortFormatRoute(route))
-			}
-			
-			
-			model.statusLog += model.startingMessage
-			
-			view.spellCheck.checkSpelling()
-		}
-		catch(Exception e)
-		{
-			model.statusLog += formatError("ERROR:<pre>${StackTrace.getStackTrace(e)}</pre>")
-		}
-		model.statusLog += "<br>\r\n"
+			model.statusLog += "<br>\r\n"
+		} //end doOutside
 	}
 	
 	def toggleOption(String optionKey)
